f01ec4dca70e27e3f8bc0c2b345ca9c9b9eef482
emmanuel-montoya <emmanuel.montoyae@gmail.com>
2023-07-22 15:50:59 -0600

    Stable commit for first cyberneom version

--- lib/profile/ui/tabs/profile_chamber_presets.dart
+++ lib/profile/ui/tabs/profile_chamber_presets.dart
@@ -30,38 +30,34 @@ class ProfileChamberPresets extends StatelessWidget {
             return GestureDetector(
               child: ListTile(
                 contentPadding: const EdgeInsets.all(8.0),
-                title: Text(chamberPreset.name.isEmpty ? "${AppTranslationConstants.frequency.tr} ${chamberPreset.neomFrequency?.frequency} Hz" : chamberPreset.name),
-                subtitle: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      chamberPreset.neomParameter != null ?
-                      Text("Vol: ${chamberPreset.neomParameter!.volume.toStringAsFixed(1)} | "
-                          "X: ${chamberPreset.neomParameter!.x.toStringAsFixed(1)} | "
-                          "Y:${chamberPreset.neomParameter!.y.toStringAsFixed(1)} | "
-                          "Z:${chamberPreset.neomParameter!.z.toStringAsFixed(1)}")
-                          : Text(AppTranslationConstants.rootFrequency.tr),
-                  RatingBar(
-                    initialRating: chamberPreset.state.toDouble(),
-                    minRating: 1,
-                    ignoreGestures: true,
-                    direction: Axis.horizontal,
-                    allowHalfRating: false,
-                    itemCount: 5,
-                    ratingWidget: RatingWidget(
-                      full: CoreUtilities.ratingImage(AppAssets.heart),
-                      half: CoreUtilities.ratingImage(AppAssets.heartHalf),
-                      empty: CoreUtilities.ratingImage(AppAssets.heartBorder),
-                    ),
-                    itemPadding: const EdgeInsets.symmetric(horizontal: 1.0),
-                    itemSize: 15,
-                    onRatingUpdate: (rating) {
-                      _.logger.d("New Rating set to $rating");
-                      },
-                  ),]),
                 leading: Hero(
-                  tag: CoreUtilities.getAppItemHeroTag(index),
-                  child: Image.network(chamberPreset.imgUrl.isNotEmpty ? chamberPreset.imgUrl
-                    : AppFlavour.getNoImageUrl(), width: 50.0)
+                    tag: CoreUtilities.getAppItemHeroTag(index),
+                    child: Image.network(chamberPreset.imgUrl.isNotEmpty ? chamberPreset.imgUrl
+                        : AppFlavour.getNoImageUrl(), width: 50.0)
+                ),
+                title: Text(chamberPreset.name.isEmpty ? "${AppTranslationConstants.frequency.tr} ${chamberPreset.neomFrequency?.frequency} Hz" : chamberPreset.name),
+                subtitle: chamberPreset.neomParameter != null ? Text("Vol: ${chamberPreset.neomParameter!.volume.toStringAsFixed(1)} | "
+                    "X: ${chamberPreset.neomParameter!.x.toStringAsFixed(1)} | "
+                    "Y:${chamberPreset.neomParameter!.y.toStringAsFixed(1)} | "
+                    "Z:${chamberPreset.neomParameter!.z.toStringAsFixed(1)}", style: const TextStyle(fontSize: 10),)
+                    : Text(AppTranslationConstants.rootFrequency.tr),
+                trailing: RatingBar(
+                  initialRating: chamberPreset.state.toDouble(),
+                  minRating: 1,
+                  ignoreGestures: true,
+                  direction: Axis.horizontal,
+                  allowHalfRating: false,
+                  itemCount: 5,
+                  ratingWidget: RatingWidget(
+                    full: CoreUtilities.ratingImage(AppAssets.heart),
+                    half: CoreUtilities.ratingImage(AppAssets.heartHalf),
+                    empty: CoreUtilities.ratingImage(AppAssets.heartBorder),
+                  ),
+                  itemPadding: const EdgeInsets.symmetric(horizontal: 1.0),
+                  itemSize: 15,
+                  onRatingUpdate: (rating) {
+                    _.logger.d("New Rating set to $rating");
+                  },
                 ),
                 onTap: () {
                   Get.toNamed(AppRouteConstants.generator,  arguments: [chamberPreset.clone()]);
